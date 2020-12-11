83f38e1ac8b8c5b1ff26dafae59726602ebd3ba8
Piskinekiisu <74102137+piskinekiisu@users.noreply.github.com>
2020-12-11 15:08:07 +0200

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,11 @@
+# menee project folderin alle 
+
+from passlib.hash import pbkdf2_sha256
+
+
+def hash_password(password):
+    return pbkdf2_sha256.hash(password)
+
+
+def check_password(password, hashed):
+    return pbkdf2_sha256.verify(password, hashed)
