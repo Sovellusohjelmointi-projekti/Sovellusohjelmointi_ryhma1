14e91cbd752b52e9a75301554d7e491532827272
JoonaPaakkonen <60470611+JoonaPaakkonen@users.noreply.github.com>
2020-12-15 16:18:04 +0200

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    DEBUG = True
-
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://admin:kiisu@localhost:5432/tuasreservations' #tätä pitää muuttaa!
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
